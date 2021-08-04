26454dacc0736cb5baafc2f1dd78d6b588e27b19
xloc <tj-xzx@126.com>
2021-08-04 01:35:04 -0700

    add change parameter interaction. with bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Svg } from '@svgdotjs/svg.js';
 import _ from 'lodash';
 import { InvoluteGear2D } from './geometry/involute_gear';
-import { Drawer, List, ListItem, makeStyles, TextField } from '@material-ui/core';
+
+// import {, List, ListItem, makeStyles, } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField'
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { makeStyles } from '@material-ui/core/styles';
 
 
 const drawerWidth = 240;
@@ -30,41 +36,91 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function App() {
 
+function useValidation<T>(
+  defaultValue: T,
+  parser: (t: string) => T,
+  validator: (v: T) => boolean
+): [string, any, T, boolean] {
+  const [text, setText] = useState(`${defaultValue}`);
+  const [value, setValue] = useState(defaultValue);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setValue(parser(text))
+  }, [text, parser])
+
+  useEffect(() => {
+    setError(!validator(value));
+  }, [value, validator])
+
+  return [text, setText, value, error];
+}
+
+
+function App() {
   const classes = useStyles();
 
-  let gear_params = {
+  const [gear_params, set_gear_params] = useState({
     n_teeth: 18,
     module: 1,
     pressure_angle_deg: 20,
     stretch_ratio: 1.2,
-  }
+  });
 
-  let g = new InvoluteGear2D(
-    gear_params.n_teeth,
-    gear_params.module,
-    gear_params.pressure_angle_deg,
-    gear_params.stretch_ratio
-  );
+  const [validated, setValidated] = useState(true);
+  const [n_teeth_text, set_n_teeth_text, n_teeth, n_teeth_error] =
+    useValidation(18, parseInt, (v) => (v >= 5));
+  const [module_text, set_module_text, module, module_error] =
+    useValidation(1, parseFloat, (v) => (v >= 0));
+  const [pressure_angle_deg_text, set_pressure_angle_deg_text, pressure_angle_deg, pressure_angle_deg_error] =
+    useValidation(20, parseFloat, (v) => (v >= 17 && v < 22));
+  const [stretch_ratio_text, set_stretch_ratio_text, stretch_ratio, stretch_ratio_error] =
+    useValidation(1, parseFloat, (v) => (v >= 1));
+  useEffect(() => {
+    setValidated(!n_teeth_error && !module_error && !pressure_angle_deg_error && !stretch_ratio_error);
+  }, [n_teeth_error, module_error, pressure_angle_deg_error, stretch_ratio_error])
 
-  // view port size
-  const vs = g.tip_diameter / 0.8;
-  const draw = new Svg()
-    .viewbox(-vs / 2, -vs / 2, vs, vs)
-    .size('100%', '100%');
 
-  const polygon = draw.polygon()
-    .fill('transparent')
-    .stroke({ color: 'tomato', width: 0.05 });
+  const [svg, set_svg] = useState(new Svg().size('100%', '100%'));
+  useEffect(() => {
+    if (n_teeth_error || module_error || pressure_angle_deg_error || stretch_ratio_error) return;
+    console.log({
+      n_teeth,
+      module,
+      pressure_angle_deg,
+      stretch_ratio
+    });
 
-  polygon.plot(g.points);
+    let g = new InvoluteGear2D(
+      n_teeth,
+      module,
+      pressure_angle_deg,
+      stretch_ratio
+    );
+
+    // view port size
+    const vs = g.tip_diameter / 0.8;
+    const draw = new Svg()
+      .viewbox(-vs / 2, -vs / 2, vs, vs)
+      .size('100%', '100%');
+
+    const polygon = draw.polygon()
+      .fill('transparent')
+      .stroke({ color: 'tomato', width: 0.05 });
+    polygon.plot(g.points);
+
+    set_svg(draw);
+  }, [
+    n_teeth, module, pressure_angle_deg, stretch_ratio, validated,
+    n_teeth_error, module_error, pressure_angle_deg_error, stretch_ratio_error
+  ]);
 
   return (
     <div className="App">
       <div className={classes.root}>
         <main className={classes.content}>
-          <div dangerouslySetInnerHTML={{ __html: draw.svg() }}></div>
+          <div dangerouslySetInnerHTML={{ __html: svg.svg() }}></div>
         </main>
         <Drawer
           className={classes.drawer}
@@ -75,15 +131,36 @@ function App() {
           anchor="right"
         >
           <List>
-
-            <ListItem button>
-              <TextField required id="standard-required" label="Required" defaultValue="Hello World" />
+            <ListItem>
+              <TextField
+                error={n_teeth_error}
+                id="standard-required" label="Number of Teeth"
+                value={n_teeth_text}
+                onChange={(e) => set_n_teeth_text(e.target.value)} />
             </ListItem>
-            <ListItem button>
-              <TextField required id="standard-required" label="Required" defaultValue="Hello World" />
+            <ListItem>
+              <TextField
+                error={module_error}
+                id="standard-required" label="Module"
+                value={module_text}
+                onChange={(e) => set_module_text(e.target.value)} />
             </ListItem>
-            <ListItem button>
-              <TextField required id="standard-required" label="Required" defaultValue="Hello World" />
+            <ListItem>
+              <TextField
+                error={pressure_angle_deg_error}
+                id="standard-required" label="Pressure Angle (deg)"
+                value={pressure_angle_deg_text}
+                onChange={(e) => set_pressure_angle_deg_text(e.target.value)} />
+            </ListItem>
+            <ListItem>
+              <TextField
+                error={stretch_ratio_error}
+                id="standard-required" label="Stretch Ratio"
+                value={stretch_ratio_text}
+                onChange={(e) => set_stretch_ratio_text(e.target.value)} />
+            </ListItem>
+            <ListItem>
+              {JSON.stringify({ validated })}
             </ListItem>
 
           </List>
